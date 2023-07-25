858caead45f447b6fbb9fd2231c4892e32d8f346
Eray Yıldız <eray1yildiz@hotmail.com>
2023-07-25 03:41:21 +0300

    build eklendi

---
+++ build/precache-manifest.440026b200f4c246e6d2daa59c78a97d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "92b199d54e702a767ce6",
+    "url": "/static/css/main.c5466ccd.chunk.css"
+  },
+  {
+    "revision": "92b199d54e702a767ce6",
+    "url": "/static/js/main.45472143.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "4b3b3e5a63e6c9e36c66",
+    "url": "/static/js/2.493677a0.chunk.js"
+  },
+  {
+    "revision": "28ea8bcf6a93ce68fea218a909445acb",
+    "url": "/static/media/logo.28ea8bcf.svg"
+  },
+  {
+    "revision": "72e9db32bda6dbdfd6da0e9d74420ad0",
+    "url": "/static/media/Barlow-Thin.72e9db32.ttf"
+  },
+  {
+    "revision": "e1143cc0e3fcb007c841e793e7c7047f",
+    "url": "/static/media/mvp-banner.e1143cc0.png"
+  },
+  {
+    "revision": "41eaef564d856a1bb1e060a5e906cf93",
+    "url": "/index.html"
+  }
+];
